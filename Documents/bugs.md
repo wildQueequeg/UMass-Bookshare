3887e0830fd44cadc2b1888626fb764092dbb98f
Walter Doan <walter.doan@gmail.com>
2015-03-07 12:16:38 -0500

    Redirect registration home button to home

---
+++ Documents/bugs.md
@@ -0,0 +1,4 @@
+Error not found bug (error with registration)
+Engine Bug
+Middleware bug
+Redirect bug
--- bookapp/app.js
+++ bookapp/app.js
@@ -18,6 +18,7 @@ app.set('view engine', 'html');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
