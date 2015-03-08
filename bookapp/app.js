a942c487059fbb269718fe7a6d1950d667543523
Walter Doan <walter.doan@gmail.com>
2015-03-08 19:23:43 -0400

    changed engine from html to ejs

--- bookapp/app.js
+++ bookapp/app.js
@@ -14,7 +14,7 @@ app.set('views', __dirname + './views');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'html');
+app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
