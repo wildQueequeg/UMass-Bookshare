8b39fdc6095a0a187839471f7e9c76373b30d0b1
kiwitheking <yukiwang9@gmail.com>
2015-03-25 13:46:05 -0400

    add mocha test

---
+++ bookapp/test/test.js
@@ -0,0 +1,24 @@
+var assert = require("assert")
+var expect = require('expect')
+var should = require('should')
+
+describe('Array', function(){
+describe('#indexOf()', function(){
+it('should return -1 when the value is not present', function(){
+assert.equal(-1, [1,2,3].indexOf(5));
+assert.equal(-1, [1,2,3].indexOf(0));
+})
+})
+})
+
+describe ('addUser', function(){
+it ('should pass',function(done){
+done();
+});
+})
+
+describe ('loginUser', function(){
+it ('should pass',function(done){
+done();
+});
+})
